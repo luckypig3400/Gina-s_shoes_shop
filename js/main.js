31176f17e670a84335e654e7005ad7bce1dff530
郵差 <a34007024@gmail.com>
2020-06-17 11:50:41 +0800

    change discount end time in main.js

--- js/main.js
+++ js/main.js
@@ -313,7 +313,7 @@
 
 	function makeTimer() {
 
-		var endTime = new Date("21 December 2019 9:56:00 GMT+01:00");			
+		var endTime = new Date("31 July 2020 00:00:00 GMT+08:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
